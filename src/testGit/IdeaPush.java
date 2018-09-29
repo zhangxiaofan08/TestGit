8969cfcf5b9b94234f5b3fcee54c02710df332b8
zhangxiaofan <42403326+zxf19930626@users.noreply.github.com>
2018-09-29 15:24:49 +0800

    idea commit

---
+++ src/testGit/IdeaPush.java
@@ -0,0 +1,11 @@
+package testGit;
+
+/**
+ * @author zhangxf
+ * @date 2018/9/29 - 15:20
+ */
+public class IdeaPush {
+    public static void main(String[] args) {
+        System.out.println("idea push");
+    }
+}
